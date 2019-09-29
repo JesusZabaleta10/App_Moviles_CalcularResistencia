9a466222541f7e049df631048ea7edcbae0e1099
Jesus Zabaleta <jesus.zabaleta@udea.edu.co>
2019-09-29 10:33:55 -0500

    Commit

--- src/main/java/com/example/calcularresistencia/MainActivity.kt
+++ src/main/java/com/example/calcularresistencia/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : AppCompatActivity() {
 
         // COLUMN 1 --------------------------------------------------------
         button5.setOnClickListener {
-            Toast.makeText(this, "Café", Toast.LENGTH_SHORT).show()
             button_banda1.setBackgroundColor(Color.parseColor("#8b4513"))
             num_C1 = 1
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -64,7 +63,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button9.setOnClickListener {
-            Toast.makeText(this, "Rojo", Toast.LENGTH_SHORT).show()
             button_banda1.setBackgroundColor(Color.parseColor("#ff0000"))
             num_C1 = 2
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -104,7 +102,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button13.setOnClickListener {
-            Toast.makeText(this, "Naranja", Toast.LENGTH_SHORT).show()
             button_banda1.setBackgroundColor(Color.parseColor("#ff8c00"))
             num_C1 = 3
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -144,7 +141,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button17.setOnClickListener {
-            Toast.makeText(this, "Amarillo", Toast.LENGTH_SHORT).show()
             button_banda1.setBackgroundColor(Color.parseColor("#ffff00"))
             num_C1 = 4
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -184,7 +180,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button21.setOnClickListener {
-            Toast.makeText(this, "Verde", Toast.LENGTH_SHORT).show()
             button_banda1.setBackgroundColor(Color.parseColor("#008000"))
             num_C1 = 5
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -224,7 +219,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button25.setOnClickListener {
-            Toast.makeText(this, "Azul", Toast.LENGTH_SHORT).show()
             button_banda1.setBackgroundColor(Color.parseColor("#0000ff"))
             num_C1 = 6
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -264,7 +258,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button29.setOnClickListener {
-            Toast.makeText(this, "Morado", Toast.LENGTH_SHORT).show()
             button_banda1.setBackgroundColor(Color.parseColor("#800080"))
             num_C1 = 7
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -304,7 +297,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button33.setOnClickListener {
-            Toast.makeText(this, "Gris", Toast.LENGTH_SHORT).show()
             button_banda1.setBackgroundColor(Color.parseColor("#808080"))
             num_C1 = 8
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -344,7 +336,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button37.setOnClickListener {
-            Toast.makeText(this, "Blanco", Toast.LENGTH_SHORT).show()
             button_banda1.setBackgroundColor(Color.parseColor("#ffffff"))
             num_C1 = 9
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -387,7 +378,6 @@ class MainActivity : AppCompatActivity() {
 
         // COLUMN 2 -----------------------------------------------------------
         button2.setOnClickListener {
-            Toast.makeText(this, "Negro", Toast.LENGTH_SHORT).show()
             button_banda2.setBackgroundColor(Color.parseColor("#000000"))
             num_C2 = 0
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -427,7 +417,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button6.setOnClickListener {
-            Toast.makeText(this, "Café", Toast.LENGTH_SHORT).show()
             button_banda2.setBackgroundColor(Color.parseColor("#8b4513"))
             num_C2 = 1
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -467,7 +456,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button10.setOnClickListener {
-            Toast.makeText(this, "Rojo", Toast.LENGTH_SHORT).show()
             button_banda2.setBackgroundColor(Color.parseColor("#ff0000"))
             num_C2 = 2
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -507,7 +495,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button14.setOnClickListener {
-            Toast.makeText(this, "Naranja", Toast.LENGTH_SHORT).show()
             button_banda2.setBackgroundColor(Color.parseColor("#ff8c00"))
             num_C2 = 3
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -547,7 +534,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button18.setOnClickListener {
-            Toast.makeText(this, "Amarillo", Toast.LENGTH_SHORT).show()
             button_banda2.setBackgroundColor(Color.parseColor("#ffff00"))
             num_C2 = 4
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -587,7 +573,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button22.setOnClickListener {
-            Toast.makeText(this, "Verde", Toast.LENGTH_SHORT).show()
             button_banda2.setBackgroundColor(Color.parseColor("#008000"))
             num_C2 = 5
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -627,7 +612,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button26.setOnClickListener {
-            Toast.makeText(this, "Azul", Toast.LENGTH_SHORT).show()
             button_banda2.setBackgroundColor(Color.parseColor("#0000ff"))
             num_C2 = 6
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -667,7 +651,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button30.setOnClickListener {
-            Toast.makeText(this, "Morado", Toast.LENGTH_SHORT).show()
             button_banda2.setBackgroundColor(Color.parseColor("#800080"))
             num_C2 = 7
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -707,7 +690,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button34.setOnClickListener {
-            Toast.makeText(this, "Gris", Toast.LENGTH_SHORT).show()
             button_banda2.setBackgroundColor(Color.parseColor("#808080"))
             num_C2 = 8
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -747,7 +729,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button38.setOnClickListener {
-            Toast.makeText(this, "Blanco", Toast.LENGTH_SHORT).show()
             button_banda2.setBackgroundColor(Color.parseColor("#ffffff"))
             num_C2 = 9
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -790,7 +771,6 @@ class MainActivity : AppCompatActivity() {
 
         // COLUMN 3 -----------------------------------------------------------
         button3.setOnClickListener {
-            Toast.makeText(this, "Negro", Toast.LENGTH_SHORT).show()
             button_banda3.setBackgroundColor(Color.parseColor("#000000"))
             multiplicador = 1
             signo = " Ω"
@@ -799,7 +779,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button7.setOnClickListener {
-            Toast.makeText(this, "Café", Toast.LENGTH_SHORT).show()
             button_banda3.setBackgroundColor(Color.parseColor("#8b4513"))
             multiplicador = 10
             signo = " Ω"
@@ -808,7 +787,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button11.setOnClickListener {
-            Toast.makeText(this, "Rojo", Toast.LENGTH_SHORT).show()
             button_banda3.setBackgroundColor(Color.parseColor("#ff0000"))
             multiplicador = 100
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -818,7 +796,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button15.setOnClickListener {
-            Toast.makeText(this, "Naranja", Toast.LENGTH_SHORT).show()
             button_banda3.setBackgroundColor(Color.parseColor("#ff8c00"))
             multiplicador = 1000
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -828,7 +805,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button19.setOnClickListener {
-            Toast.makeText(this, "Amarillo", Toast.LENGTH_SHORT).show()
             button_banda3.setBackgroundColor(Color.parseColor("#ffff00"))
             multiplicador = 10000
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -838,7 +814,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button23.setOnClickListener {
-            Toast.makeText(this, "Verde", Toast.LENGTH_SHORT).show()
             button_banda3.setBackgroundColor(Color.parseColor("#008000"))
             multiplicador = 100000
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -848,7 +823,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button27.setOnClickListener {
-            Toast.makeText(this, "Azul", Toast.LENGTH_SHORT).show()
             button_banda3.setBackgroundColor(Color.parseColor("#0000ff"))
             multiplicador = 1000000
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -858,7 +832,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button31.setOnClickListener {
-            Toast.makeText(this, "Morado", Toast.LENGTH_SHORT).show()
             button_banda3.setBackgroundColor(Color.parseColor("#800080"))
             multiplicador = 10000000
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -868,7 +841,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button35.setOnClickListener {
-            Toast.makeText(this, "Gris", Toast.LENGTH_SHORT).show()
             button_banda3.setBackgroundColor(Color.parseColor("#808080"))
             multiplicador = 100000000
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -878,7 +850,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button39.setOnClickListener {
-            Toast.makeText(this, "Blanco", Toast.LENGTH_SHORT).show()
             button_banda3.setBackgroundColor(Color.parseColor("#ffffff"))
             multiplicador = 1000000000
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -888,7 +859,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button43.setOnClickListener {
-            Toast.makeText(this, "Dorado", Toast.LENGTH_SHORT).show()
             button_banda3.setBackgroundColor(Color.parseColor("#b8860b"))
             var concatenar = num_C1.toString() + num_C2.toString()
             var resultado = concatenar.toDouble()/10
@@ -896,7 +866,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button47.setOnClickListener {
-            Toast.makeText(this, "Plata", Toast.LENGTH_SHORT).show()
             button_banda3.setBackgroundColor(Color.parseColor("#c0c0c0"))
             var concatenar = num_C1.toString() + num_C2.toString()
             var resultado = concatenar.toDouble()/100
@@ -907,7 +876,6 @@ class MainActivity : AppCompatActivity() {
 
         // COLUMN 4 -----------------------------------------------------------
         button8.setOnClickListener {
-            Toast.makeText(this, "Café", Toast.LENGTH_SHORT).show()
             button_banda4.setBackgroundColor(Color.parseColor("#8b4513"))
             tolerancia = " ± 1 %"
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -947,7 +915,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button12.setOnClickListener {
-            Toast.makeText(this, "Rojo", Toast.LENGTH_SHORT).show()
             button_banda4.setBackgroundColor(Color.parseColor("#ff0000"))
             tolerancia = " ± 2 %"
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -987,7 +954,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button24.setOnClickListener {
-            Toast.makeText(this, "Verde", Toast.LENGTH_SHORT).show()
             button_banda4.setBackgroundColor(Color.parseColor("#008000"))
             tolerancia = " ± 0.5 %"
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -1027,7 +993,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button28.setOnClickListener {
-            Toast.makeText(this, "Azul", Toast.LENGTH_SHORT).show()
             button_banda4.setBackgroundColor(Color.parseColor("#0000ff"))
             tolerancia = " ± 0.25 %"
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -1067,7 +1032,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button32.setOnClickListener {
-            Toast.makeText(this, "Morado", Toast.LENGTH_SHORT).show()
             button_banda4.setBackgroundColor(Color.parseColor("#800080"))
             tolerancia = " ± 0.1 %"
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -1107,7 +1071,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button36.setOnClickListener {
-            Toast.makeText(this, "Gris", Toast.LENGTH_SHORT).show()
             button_banda4.setBackgroundColor(Color.parseColor("#808080"))
             tolerancia = " ± 0.05 %"
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -1147,7 +1110,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button44.setOnClickListener {
-            Toast.makeText(this, "Dorado", Toast.LENGTH_SHORT).show()
             button_banda4.setBackgroundColor(Color.parseColor("#b8860b"))
             tolerancia = " ± 5 %"
             var concatenar = num_C1.toString() + num_C2.toString()
@@ -1187,7 +1149,6 @@ class MainActivity : AppCompatActivity() {
             tv_resultado.text = resultado.toString() + signo + tolerancia
         }
         button48.setOnClickListener {
-            Toast.makeText(this, "Plata", Toast.LENGTH_SHORT).show()
             button_banda4.setBackgroundColor(Color.parseColor("#c0c0c0"))
             tolerancia = " ± 10 %"
             var concatenar = num_C1.toString() + num_C2.toString()
